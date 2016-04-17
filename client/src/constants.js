426a45fb5e7839a2ebfdd1d40ca78da0f7186a4f
Simon Elliott <simon@timecounts.org>
2016-04-17 11:37:49 +0100

    commonjs. BRING BACK key-mirror!

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,41 +1,42 @@
-export const PHASE_LOBBY = "PHASE_LOBBY";
-export const PHASE_REVEAL = "PHASE_REVEAL";
-export const PHASE_DAY = "PHASE_DAY";
-export const PHASE_VOTE = "PHASE_VOTE";
-export const PHASE_NIGHT = "PHASE_NIGHT";
-export const PHASE_END = "PHASE_END";
+exports.PHASE_LOBBY = "PHASE_LOBBY";
+exports.PHASE_REVEAL = "PHASE_REVEAL";
+exports.PHASE_DAY = "PHASE_DAY";
+exports.PHASE_VOTE = "PHASE_VOTE";
+exports.PHASE_NIGHT = "PHASE_NIGHT";
+exports.PHASE_END = "PHASE_END";
 
-export const CREATE_GAME = "CREATE_GAME";
-export const START_GAME = "START_GAME";
-export const JOIN_GAME = "JOIN_GAME";
-export const REVEAL_READY = "REVEAL_READY";
-export const NOMINATE = "NOMINATE";
-export const VOTE_YES = "VOTE_YES";
-export const VOTE_NO = "VOTE_NO";
-export const DEVOUR = "DEVOUR";
-export const UPDATE_STATE = "UPDATE_STATE";
+exports.CREATE_GAME = "CREATE_GAME";
+exports.START_GAME = "START_GAME";
+exports.JOIN_GAME = "JOIN_GAME";
+exports.REVEAL_READY = "REVEAL_READY";
+exports.NOMINATE = "NOMINATE";
+exports.VOTE_YES = "VOTE_YES";
+exports.VOTE_NO = "VOTE_NO";
+exports.DEVOUR = "DEVOUR";
+exports.UPDATE_STATE = "UPDATE_STATE";
+exports.PLAYER_PRESENCE = "PLAYER_PRESENCE";
 
-export const WEREWOLF = "WEREWOLF";
-export const VILLAGER = "VILLAGER";
+exports.WEREWOLF = "WEREWOLF";
+exports.VILLAGER = "VILLAGER";
 
-export const PAGE_MENU = "PAGE_MENU";
-export const PAGE_HOST = "PAGE_HOST";
-export const PAGE_JOIN  = "PAGE_JOIN";
-export const PAGE_LOBBY = "PAGE_LOBBY";
-export const PAGE_REVEAL = "PAGE_REVEAL";
-export const PAGE_NIGHT = "PAGE_NIGHT";
-export const PAGE_VOTE = "PAGE_VOTE";
-export const PAGE_DAY = "PAGE_DAY";
-export const PAGE_END = "PAGE_END";
+exports.PAGE_MENU = "PAGE_MENU";
+exports.PAGE_HOST = "PAGE_HOST";
+exports.PAGE_JOIN  = "PAGE_JOIN";
+exports.PAGE_LOBBY = "PAGE_LOBBY";
+exports.PAGE_REVEAL = "PAGE_REVEAL";
+exports.PAGE_NIGHT = "PAGE_NIGHT";
+exports.PAGE_VOTE = "PAGE_VOTE";
+exports.PAGE_DAY = "PAGE_DAY";
+exports.PAGE_END = "PAGE_END";
 
-export const ERROR = "ERROR";
+exports.ERROR = "ERROR";
 
-export const SHOW_HOST = "SHOW_HOST";
-export const SHOW_JOIN = "SHOW_JOIN";
+exports.SHOW_HOST = "SHOW_HOST";
+exports.SHOW_JOIN = "SHOW_JOIN";
 
-export const GAME_STATE_UPDATE = "GAME_STATE_UPDATE";
-export const CONNECTING = "CONNECTING";
-export const CONNECTED = "CONNECTED";
-export const HOST_GAME = "HOST_GAME";
-export const CANCEL = "CANCEL";
-export const CHOOSE_VICTIM = "CHOOSE_VICTIM";
+exports.GAME_STATE_UPDATE = "GAME_STATE_UPDATE";
+exports.CONNECTING = "CONNECTING";
+exports.CONNECTED = "CONNECTED";
+exports.HOST_GAME = "HOST_GAME";
+exports.CANCEL = "CANCEL";
+exports.CHOOSE_VICTIM = "CHOOSE_VICTIM";
