b0f06959f8120340919f345780ab0fa3a2b71b1e
Thomas Parslow <tom@almostobsolete.net>
2016-04-17 11:16:23 +0100

    Key mirror *shakes fist*

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,47 +1,41 @@
-const keyMirror = require('key-mirror');
+export const PHASE_LOBBY = "PHASE_LOBBY";
+export const PHASE_REVEAL = "PHASE_REVEAL";
+export const PHASE_DAY = "PHASE_DAY";
+export const PHASE_VOTE = "PHASE_VOTE";
+export const PHASE_NIGHT = "PHASE_NIGHT";
+export const PHASE_END = "PHASE_END";
 
-module.exports = keyMirror({
-  PHASE_LOBBY: null,
-  PHASE_REVEAL: null,
-  PHASE_DAY: null,
-  PHASE_VOTE: null,
-  PHASE_NIGHT: null,
-  PHASE_END: null,
+export const CREATE_GAME = "CREATE_GAME";
+export const START_GAME = "START_GAME";
+export const JOIN_GAME = "JOIN_GAME";
+export const REVEAL_READY = "REVEAL_READY";
+export const NOMINATE = "NOMINATE";
+export const VOTE_YES = "VOTE_YES";
+export const VOTE_NO = "VOTE_NO";
+export const DEVOUR = "DEVOUR";
+export const UPDATE_STATE = "UPDATE_STATE";
 
-  CREATE_GAME: null,
-  START_GAME: null,
-  JOIN_GAME: null,
-  REVEAL_READY: null,
-  NOMINATE: null,
-  VOTE_YES: null,
-  VOTE_NO: null,
-  DEVOUR: null,
-  UPDATE_STATE: null,
+export const WEREWOLF = "WEREWOLF";
+export const VILLAGER = "VILLAGER";
 
-  WEREWOLF: null,
-  VILLAGER: null,
+export const PAGE_MENU = "PAGE_MENU";
+export const PAGE_HOST = "PAGE_HOST";
+export const PAGE_JOIN  = "PAGE_JOIN";
+export const PAGE_LOBBY = "PAGE_LOBBY";
+export const PAGE_REVEAL = "PAGE_REVEAL";
+export const PAGE_NIGHT = "PAGE_NIGHT";
+export const PAGE_VOTE = "PAGE_VOTE";
+export const PAGE_DAY = "PAGE_DAY";
+export const PAGE_END = "PAGE_END";
 
-  PAGE_MENU: null,
-  PAGE_HOST: null,
-  PAGE_JOIN : null,
-  PAGE_LOBBY: null,
-  PAGE_REVEAL: null,
-  PAGE_NIGHT: null,
-  PAGE_VOTE: null,
-  PAGE_DAY: null,
-  PAGE_END: null,
+export const ERROR = "ERROR";
 
-  ERROR: null,
+export const SHOW_HOST = "SHOW_HOST";
+export const SHOW_JOIN = "SHOW_JOIN";
 
-  SHOW_HOST: null,
-  SHOW_JOIN: null,
-
-  GAME_STATE_UPDATE: null,
-  JOIN_GAME: null,
-  CONNECTING: null,
-  CONNECTED: null,
-  HOST_GAME: null,
-  CANCEL: null,
-  CHOOSE_VICTIM: null,
-
-});
+export const GAME_STATE_UPDATE = "GAME_STATE_UPDATE";
+export const CONNECTING = "CONNECTING";
+export const CONNECTED = "CONNECTED";
+export const HOST_GAME = "HOST_GAME";
+export const CANCEL = "CANCEL";
+export const CHOOSE_VICTIM = "CHOOSE_VICTIM";
